5143208639e318820c2d29b55f4a32d8089b4315
Marcs4-hupt <mackmarck3@gmail.com>
2025-02-21 09:25:21 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&wCl22TDQ#z-_1L7r1p5-Voo1hhUhMF4QP5OF1DGH-YCP3M_TMpng' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
